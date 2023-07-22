7827bdd9939834658e67128b52a32dad82e7e89f
marlawn <marlonvergara12@gmail.com>
2023-07-22 01:20:44 -0400

    IDKK

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
             <h class="heading"> &lt;projects /&gt; </h>
             <ul class="list">
                 <li><a href="https://github.com/marlawn/doodle-jump-clone">Doodle Jump Clone</a></li>
-                <li><a href="https://github.com/marlawn/marlawn/tree/master">First Portfolio Website</a></li>
+                <li><a href="https://github.com/marlawn/first-portfolio-website">First Portfolio Website</a></li>
                 <li><a href="https://github.com/marlawn/POS-System-Android">POS System Android</a></li>
                 <li><a href="https://github.com/marlawn/GUI-Programming-with-Navigation">POS System JavaFX</a></li>
             </ul>
