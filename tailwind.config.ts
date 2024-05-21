e4683f35f1cf5adb7b6b659e2e8e8239ff9320a9
Marlon Vergara <93688231+marlawn@users.noreply.github.com>
2024-05-21 16:22:28 -0400

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "black-bg": "url('data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22%3E%3Crect width=%22100%25%22 height=%22100%25%22 fill=%22black%22/%3E%3C/svg%3E')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
